35dceec8a76abe2fa344b0ef78e975447644eb6a
Gauri Lokhande <157399853+lokhandegauri123@users.noreply.github.com>
2026-03-23 16:43:51 +0530

    fetching specific post feature added

--- src/controller/post.controller.js
+++ src/controller/post.controller.js
@@ -32,7 +32,7 @@ async function createPostController(req, res) {
     const file = await imagekit.files.upload({
       file: stream,
       fileName: req.file.originalname,
-      folder: '/posts',
+      folder: "/posts",
       useUniqueFileName: true,
     });
 
@@ -55,6 +55,74 @@ async function createPostController(req, res) {
   }
 }
 
+async function getPostController(req, res) {
+  const token = req.cookies?.token;
+
+  let decoded;
+  try {
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
+  } catch (err) {
+    return res.status(400).json({
+      message: "Token unauthorized",
+    });
+  }
+
+  const userId = decoded.id;
+  const posts = await postModel
+    .find({
+      user: userId,
+    })
+    .lean();
+
+  res.status(200).json({
+    message: "post fetched successfully",
+    posts,
+  });
+}
+
+/* get  /api/posts/details/:postId
+
+it return specific post and also check whether it belong to specific person or not 
+:postId = dynamic id
+*/
+async function getPostDetailsController(req, res) {
+  const token = req.cookies?.token;
+
+  let decoded;
+  try {
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
+  } catch (err) {
+    return res.status(401).json({
+      message: "invalid Token ",
+    });
+  }
+
+  const userId = decoded.id;
+  const postId = req.params.postId;
+
+  const post = await postModel.findById(postId);
+  if (!post) {
+    return res.status(404).json({
+      message: "post not found",
+    });
+  }
+
+  const isValidUser = post.user.toString() === userId;
+
+  if (!isValidUser) {
+    return res.status(403).json({
+      message: "Forbidden content",
+    });
+  }
+
+  return res.status(200).json({
+    message: "post fetched successfully",
+    post,
+  });
+}
+
 module.exports = {
   createPostController,
+  getPostController,
+  getPostDetailsController,
 };
